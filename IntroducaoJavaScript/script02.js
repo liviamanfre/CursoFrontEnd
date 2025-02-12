26569f1c0d01e979ec9940748c21e331d23a6617
liviamanfre <manfrelivia03@gmail.com>
2025-02-12 15:20:09 -0300

    12.02.2025

---
+++ IntroducaoJavaScript/script02.js
@@ -0,0 +1,25 @@
+// Estrutura de dados 
+
+// Condicionais  ( If Else/ switch Case)
+// if
+var precoPrduto = 150;
+if (precoProduto >=100){
+    console.log("Valor a Pagar: "+(precoProduto*0,9));
+} else {
+    console.log("Valor a Pagar: "+(precoProduto));
+}
+
+//Switch Case
+switch (mes) {
+    case 1:
+        console.log("Janeiro");
+        break;
+    case 2:
+        console.log("Fevereiro"); 
+        break;
+    case 3:
+        console.log("Março");   
+        break;
+    default:
+        console.log("Outro Mês"); 
+}
