cc56a03dd183a9728c4f38a1d1d0e2ee16ae0990
liviamanfre <manfrelivia03@gmail.com>
2025-02-25 11:07:39 -0300

    25.02.2025

--- ArrayMatrix/script00.js
+++ ArrayMatrix/script00.js
@@ -36,3 +36,41 @@ console.log(arrayTexto);
 for(let i =0;i<arrayNumeros.length;i++){
     console.log("índice["+i+"]="+[arrayNumeros[i]])
 }
+
+//forEach
+arrayTexto.forEach(elemento => {
+    console.log(elemento)
+});
+
+//Métodos útei
+
+//IndexOf
+console.log(arrayNumeros.indexOf(5)); //4
+console.log(arrayNumeros.indexOf(10)); //-1 (elemento inesistente)
+
+//Splice (remover elementos da posição)
+arrayMisto.splice(2,1)
+console.log(arrayMisto);
+
+//Operações Avançadas de Arrays
+// map - comparação
+let valores = [10, 20, 30, 40, 50,];
+let valoresDobro = valores.map(x => x*2);
+console.log(valoresDobro);
+// filter - comparação 
+let valoresFilter = valoresFilter(x => x>25);
+console.log(valoresFilter);
+
+//Desafio (filtro x<35) && (x*3) = [30, 60, 90]
+let valoresDesafio = valores.filter(x=>x<35).
+map(x=>x*3); 
+
+//reduce ([] -> let x)
+let soma = valores.reduce((x,y) => x+y); 
+console.log(soma);
+
+//sort (organizar)
+let aleatorio = [7, 4, 2, 9, 1, 5, 8, 3, 6];
+console.log(aleatorio);
+let ordenado = aleatório.sort();
+console.log(ordenado);
