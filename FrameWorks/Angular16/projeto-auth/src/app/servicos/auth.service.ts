08d977c57b0113c6ce6facf9cfbc9a8bd507eee4
liviamanfre <manfrelivia03@gmail.com>
2025-08-27 15:47:51 -0300

    27.08.2026

--- FrameWorks/Angular16/projeto-auth/src/app/servicos/auth.service.ts
+++ FrameWorks/Angular16/projeto-auth/src/app/servicos/auth.service.ts
@@ -7,44 +7,65 @@ import { Observable, switchMap, throwError, map } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3016/usuarios';
+  private apiUrl = 'http://localhost:3000/usuarios';
   private readonly CHAVE_AUTH = 'usuarioLogado';
+
 
   constructor(private router: Router, private http: HttpClient) { }
 
+  //função para registrar um novo usuário
   registrar(usuario: any):Observable<any> {
+    //antes de registrar  -> verificar se usuário já esta cadastrado
     return this.http.get<any[]>(`${this.apiUrl}?email=${usuario.email}`).pipe(
       switchMap(res => {
+        // se emial já existe
         if(res.length>0){
-          return throwError(()=>new Error ("Usuário Já Existe."));
+          // criando um erro para o sistema -> para que o erro seja tratado, rodar a função dentro de um try/catch
+          return throwError(()=>new Error ("Usuário Já Existe.")); // para o programa aqui, se usuário já existir
         }
+        //caso contrário -> cadastrar o usuário novo
         return this.http.post<any>(this.apiUrl, usuario);
       })
     );
   }
 
+  //função para logar o usuário
   login(credenciais: any):Observable<boolean>{
-    return this.http.get<any[]>(`${this.apiUrl}?email=${credenciais.email}&senha=${credenciais.senha}`).pipe(
-      map(usuario => {
-        if (usuario.length > 0) {
-          localStorage.setItem(this.CHAVE_AUTH, JSON.stringify(usuario[0]));
-          return true;
-        }
-        return false;
-      })
-    )
-  }
 
+    return this.http.get<any[]>(
+      // passar o email e a senha para procurar o usuário no BD
+      `${this.apiUrl}?email=${credenciais.email}&senha=${credenciais.senha}`).pipe(
+        map(usuario => {
+          //usuário encontrado
+          if (usuario.length > 0) {
+            // grava no aramazenamento interno do navegador(a chave de autenticação e as informaç~eos do usuário)
+            localStorage.setItem(this.CHAVE_AUTH, JSON.stringify(usuario[0]));
+            return true;
+          }
+          //caso não seja encontrado -> retorna falso
+          return false;
+        })
+    );
+  }
+  // função de logout
   logout(){
+    // limpo o aramazenamento interno
     localStorage.removeItem(this.CHAVE_AUTH);
-    this.router.navigate(["/login"]); 
+    //redireciona para tela de login / home
+    this.router.navigate(["/login"]);
   }
 
+  // função para verificar se o usuário está autenticado
   estaAutenticado(): boolean {
-    return !!localStorage.getItem(this.CHAVE_AUTH); 
+    //uso de dupla negação => transformo em uma booleana
+    //!! -> se retornar vazio -> falso, agora se retornar texto -> é true
+    return !!localStorage.getItem(this.CHAVE_AUTH);
   }
 
+  //função apra pegar as informaç~eos do usuário
   usuarioAtual(): any{
+    // converte para json as informações do usuário
     return JSON.parse(localStorage.getItem(this.CHAVE_AUTH) || "{}")
   }
+
 }
